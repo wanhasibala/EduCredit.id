b71b5c794dd8c2174a680e1e9764af08cfe68ed7
wanhasibala <wanhasibal1122@gmail.com>
2023-09-03 17:31:34 +0700

    adding some commit to new laptop

--- screens/CourseDetail.js
+++ screens/CourseDetail.js
@@ -1,9 +1,14 @@
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { useLayoutEffect } from "react";
 import { COURSES } from "../data & models/data";
 
 function CourseDetail({route}) {
 
-  return <Text>anjay</Text>;
+  return <Text>anjay 123</Text>;
 }
 export default CourseDetail;
+const styles = StyleSheet.create({
+  container: {
+    color: "#f5f0f0"
+  }
+})
